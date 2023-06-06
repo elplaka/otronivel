f6c242db264758b1c5faeaa26626d09550cdbd53
elplaka <elplaka@hotmail.com>
2023-06-06 12:15:33 -0700

    Apoyo

--- app/Http/Controllers/ApoyoController.php
+++ app/Http/Controllers/ApoyoController.php
@@ -47,6 +47,7 @@ class ApoyoController extends Controller
             ->where('id_remesa', $id_remesa)
             ->where('estudiantes.id_ciclo', $ciclo)
             ->where('estudiantes.cve_ciudad_escuela', 1)->where('estudiantes.cve_status', 7)
+            ->where('apoyos_montos.monto', '>', 0)
             ->orderBy('estudiantes.primer_apellido')
             ->orderBy('estudiantes.segundo_apellido')
             ->orderBy('estudiantes.nombre'); 
@@ -65,6 +66,7 @@ class ApoyoController extends Controller
             ->where('id_remesa', $id_remesa)
             ->where('estudiantes.id_ciclo', $ciclo)
             ->where('estudiantes.cve_ciudad_escuela', 2)->where('estudiantes.cve_status', 6)
+            ->where('apoyos_montos.monto', '>', 0)
             ->orderBy('estudiantes.primer_apellido')
             ->orderBy('estudiantes.segundo_apellido')
             ->orderBy('estudiantes.nombre'); 
